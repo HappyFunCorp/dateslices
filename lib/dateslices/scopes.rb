f1f516dec860c14d375da9168155f8c5825de208
Matt <matt@imac.home>
2014-10-25 18:36:03 +0100

    minor refactoring of scopes.rb, sinlge-quote .gemspec

--- lib/dateslices/scopes.rb
+++ lib/dateslices/scopes.rb
@@ -4,24 +4,20 @@ module Dateslices
     Dateslices::FIELDS.each do |field|
       define_method :"group_by_#{field}" do |*args|
         args = args.dup
+        column = args[0].blank? ? 'created_at' : args[0]
+        sql = ['count(*) as count']
 
-        column = args[0]
-        column = 'created_at' if column.blank?
-
-        sql = ['count(*) as cnt']
-
-        time_filter = ''
-
-        case connection.adapter_name
-        when 'SQLite'
-          time_filter = Dateslices::Sqlite.time_filter(column, field)
-        when 'PostgreSQL', 'PostGIS'
-          time_filter = Dateslices::Postgresql.time_filter(column, field)
-        when 'MySQL'
-          time_filter = Dateslices::Mysql.time_filter(column, field)
-        else
-          throw "Unknown database adaptor #{connection.adapter_name}"
-        end
+        time_filter = case connection.adapter_name
+                        when 'SQLite'
+                          Dateslices::Sqlite.time_filter(column, field)
+                        when 'PostgreSQL', 'PostGIS'
+                          Dateslices::Postgresql.time_filter(column, field)
+                        when 'MySQL'
+                          Dateslices::Mysql.time_filter(column, field)
+                        else
+                          ''
+                          throw "Unknown database adaptor #{connection.adapter_name}"
+                        end
 
         sql << "#{time_filter} as date_slice"
 
@@ -29,7 +25,7 @@ module Dateslices
           slice = c['date_slice']
           slice = slice.to_i.to_s if slice.is_a? Float
           slice = slice.to_s if slice.is_a? Integer
-          { date_slice: slice, count: c["cnt"] }
+          { date_slice: slice, count: c['count'] }
         end
       end
     end
